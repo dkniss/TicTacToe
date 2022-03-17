365d48e070f37b6f8561ddf739dd09df85080956
Daniil Kniss <knissdaniil@gmail.com>
2022-03-17 18:15:09 +0300

    refactoring game ended state

--- XO-game/State/GameEndedState.swift
+++ XO-game/State/GameEndedState.swift
@@ -21,45 +21,15 @@ class GameEndedState: GKState {
     
     // MARK: - Methods
     override func didEnter(from previousState: GKState?) {
+        setupUI()
+        
         if let playerInput = previousState as? PlayerInputState, playerInput.isWinner {
             recordEvent(.endGame(winner: playerInput.player))
-            var message = ""
-            
-            switch playerInput.player {
-            case .first:
-                message = "первый"
-            case .second:
-                message = "второй"
-            }
-            
-            self.gameViewController.winnerLabel.text = self.winnerName(from: playerInput.player) + " win"
-            let alertVC = UIAlertController(title: "Игра окончена!", message: "Победил \(message) игрок", preferredStyle: .alert)
-            let action = UIAlertAction(title: "Ок", style: .default) { _ in
-                self.gameViewController.startNewGame()
-            }
-            alertVC.addAction(action)
-            self.gameViewController.present(alertVC, animated: true)
+            showWinner(player: playerInput.player)
         } else if let playerInput = previousState as? AllTurnsDoneState {
-            recordEvent(.endGame(winner: playerInput.player!))
-            
-            var message = ""
-            
-            switch playerInput.player {
-            case .first:
-                message = "первый"
-            case .second:
-                message = "второй"
-            default:
-                message = ""
-            }
-            
-            self.gameViewController.winnerLabel.text = self.winnerName(from: playerInput.player!) + " win"
-            let alertVC = UIAlertController(title: "Игра окончена!", message: "Победил \(message) игрок", preferredStyle: .alert)
-            let action = UIAlertAction(title: "Ок", style: .default) { _ in
-                self.gameViewController.startNewGame()
-            }
-            alertVC.addAction(action)
-            self.gameViewController.present(alertVC, animated: true)
+            guard let player = playerInput.player else { return }
+            recordEvent(.endGame(winner: player))
+            showWinner(player: player)
         } else {
             self.gameViewController.winnerLabel.text = "Ничья"
             let alertVC = UIAlertController(title: "Игра окончена", message: "Ничья", preferredStyle: .alert)
@@ -69,7 +39,6 @@ class GameEndedState: GKState {
             alertVC.addAction(action)
             self.gameViewController.present(alertVC, animated: true)
         }
-        
     }
     
     override func isValidNextState(_ stateClass: AnyClass) -> Bool {
@@ -88,5 +57,18 @@ class GameEndedState: GKState {
         self.gameViewController.winnerLabel.isHidden = false
         self.gameViewController.firstPlayerTurnLabel.isHidden = true
         self.gameViewController.secondPlayerTurnLabel.isHidden = true
+    }
+    
+    private func showWinner(player: Player) {
+        self.gameViewController.winnerLabel.text = self.winnerName(from: player) + " win"
+        
+        let message = player == .first ? "первый" : "второй"
+        
+        let alertVC = UIAlertController(title: "Игра окончена!", message: "Победил \(message) игрок", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ок", style: .default) { _ in
+            self.gameViewController.startNewGame()
+        }
+        alertVC.addAction(action)
+        self.gameViewController.present(alertVC, animated: true)
     }
 }
